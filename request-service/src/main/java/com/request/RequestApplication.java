660e6689edde83bb164328560ce7a14f8fa0729c
Luciano Mardonio Lopez Duran <lucianolopez4000@hotmail.com>
2022-07-04 17:10:52 -0500

    add

--- request-service/src/main/java/com/request/RequestApplication.java
+++ request-service/src/main/java/com/request/RequestApplication.java
@@ -6,14 +6,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
 
 @EnableEurekaClient
 @SpringBootApplication
 @EnableHystrix
 @EnableHystrixDashboard
 @EnableFeignClients
-@FeignClient
 public class RequestApplication {
 
 	public static void main(String[] args) {
