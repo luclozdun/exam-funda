f065ecf7240356d8ab16bfa3637a9ba6a0c9568d
Luciano Mardonio Lopez Duran <lucianolopez4000@hotmail.com>
2022-07-04 16:38:10 -0500

    add

--- employee-service/src/main/java/com/user/EmployeeApplication.java
+++ employee-service/src/main/java/com/user/EmployeeApplication.java
@@ -1,19 +1,21 @@
-package com.request;
+package com.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
 @SpringBootApplication
 @EnableHystrix
 @EnableHystrixDashboard
-public class RequestApplication {
+@EnableFeignClients
+public class EmployeeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RequestApplication.class, args);
+		SpringApplication.run(EmployeeApplication.class, args);
 	}
 
 }
